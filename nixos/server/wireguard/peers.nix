8cf1df0af3ef5888d60f127af1a017ef56aabc27
Gaetan Lepage <gaetan@glepage.com>
2024-04-16 17:49:37 +0200

    nixos/server/wireguard: peers in separate file

---
+++ nixos/server/wireguard/peers.nix
@@ -0,0 +1,59 @@
+[
+  ##########
+  # Gaetan #
+  ##########
+
+  # tuxedo laptop
+  {
+    publicKey = "OWezLnTrXzr1YFRs96urcqkOF6J+55S3c1NI0Jq4AXk=";
+    allowedIPs = ["10.10.10.2/32"];
+  }
+  # Phone
+  {
+    publicKey = "01dWWmsHjpNH7vCEioE3RkSl71zBVM6iSG+Vbb4yix0=";
+    allowedIPs = ["10.10.10.3/32"];
+  }
+  # alya
+  {
+    publicKey = "hwt8e4sb0IkcrIhe/IBkefZjpa9LcRp5OUoKs569nCY=";
+    allowedIPs = ["10.10.10.4/32"];
+  }
+  # cuda
+  {
+    publicKey = "J+STSrQtKJQoUNykoVF3c9ngaVUkMO3FLefQjIX1qBw=";
+    allowedIPs = ["10.10.10.5/32"];
+  }
+  # chamaeleon
+  {
+    publicKey = "VtmnRPYRg0iKTaC/HE4gQ/PelAhtC/dcsz7bJ5ktMVE=";
+    allowedIPs = ["10.10.10.6/32"];
+  }
+
+  #################
+  # Lepage family #
+  #################
+
+  # ACL desktop
+  {
+    publicKey = "qU0iNknugLhQpl5YrQaVL1D+i0AdNPUuVtJXAZojcxI=";
+    allowedIPs = ["10.10.10.20/32"];
+  }
+
+  # MacBook FL
+  {
+    publicKey = "h9/IH7UOiK0d9NXZ1liIsiJrQoSzJa5TqGFC0rQnKw0=";
+    allowedIPs = ["10.10.10.21/32"];
+  }
+
+  # Laptop TL
+  {
+    publicKey = "sHlIzQ1NMKcTQfr30Ss+LtqbauBySpQN4+WvW5SOXXI=";
+    allowedIPs = ["10.10.10.22/32"];
+  }
+
+  # Feroe
+  {
+    publicKey = "GXT4bebPRfYqR3cvODMZsR2GdCsldnAe6BnkMhI6rTs=";
+    allowedIPs = ["10.10.10.23/32"];
+  }
+]
