f4048650c41d1ff34fe4fa96daccb9c12f1e800c
Gaetan Lepage <gaetan@glepage.com>
2024-06-27 14:22:14 +0200

    nixos/nextcloud: fix webserver

--- nixos/server/nextcloud/default.nix
+++ nixos/server/nextcloud/default.nix
@@ -32,45 +32,61 @@ in {
       "listen.group" = config.services.caddy.group;
     };
 
-    caddy.virtualHosts.${hostname} = {
+    # Source: https://github.com/onny/nixos-nextcloud-testumgebung/blob/main/nextcloud-extras.nix#L117-L167
+    caddy.virtualHosts.${hostname} = let
+      webroot = config.services.nginx.virtualHosts.${hostname}.root;
+    in {
       extraConfig = ''
-        root * ${config.services.nextcloud.package}
-        file_server
+        encode zstd gzip
 
-        php_fastcgi unix/${config.services.phpfpm.pools.nextcloud.socket}
+        root * ${webroot}
+
+        redir /.well-known/carddav /remote.php/dav 301
+        redir /.well-known/caldav /remote.php/dav 301
+        redir /.well-known/* /index.php{uri} 301
+        redir /remote/* /remote.php{uri} 301
 
         header {
-            # enable HSTS
-            Strict-Transport-Security max-age=31536000;
+            Strict-Transport-Security max-age=31536000
+            Permissions-Policy interest-cohort=()
+            X-Content-Type-Options nosniff
+            X-Frame-Options SAMEORIGIN
+            Referrer-Policy no-referrer
+            X-XSS-Protection "1; mode=block"
+            X-Permitted-Cross-Domain-Policies none
+            X-Robots-Tag "noindex, nofollow"
+            -X-Powered-By
         }
 
-        # Apps paths
-        handle /nix-apps/* {
-            root * ${config.services.nextcloud.home}
-        }
-        handle /store-apps/* {
-            root * ${config.services.nextcloud.home}
+        php_fastcgi unix/${config.services.phpfpm.pools.nextcloud.socket} {
+            root ${webroot}
+            env front_controller_active true
+            env modHeadersAvailable true
         }
 
-        redir /.well-known/carddav /remote.php/dav/ 301
-        redir /.well-known/caldav /remote.php/dav/ 301
-
-        # .htaccess / data / config / ... shouldn't be accessible from outside
         @forbidden {
-            path    /.htaccess
-            path    /data/*
-            path    /config/*
-            path    /db_structure
-            path    /.xml
-            path    /README
-            path    /3rdparty/*
-            path    /lib/*
-            path    /templates/*
-            path    /occ
-            path    /console.php
+            path /build/* /tests/* /config/* /lib/* /3rdparty/* /templates/* /data/*
+            path /.* /autotest* /occ* /issue* /indie* /db_* /console*
+            not path /.well-known/*
         }
+        error @forbidden 404
 
-        respond @forbidden 404
+        @immutable {
+            path *.css *.js *.mjs *.svg *.gif *.png *.jpg *.ico *.wasm *.tflite
+            query v=*
+        }
+        header @immutable Cache-Control "max-age=15778463, immutable"
+
+        @static {
+            path *.css *.js *.mjs *.svg *.gif *.png *.jpg *.ico *.wasm *.tflite
+            not query v=*
+        }
+        header @static Cache-Control "max-age=15778463"
+
+        @woff2 path *.woff2
+        header @woff2 Cache-Control "max-age=604800"
+
+        file_server
       '';
     };
 
