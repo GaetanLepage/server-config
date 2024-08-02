26b6c00811723e257f1b77337e8c5a73cad6800c
Gaetan Lepage <gaetan@glepage.com>
2024-08-02 10:06:54 +0200

    nixos/vps: add age pubkey

--- nixos/vps/default.nix
+++ nixos/vps/default.nix
@@ -1,5 +1,4 @@
-{lib, ...}:
-{
+{lib, ...}: {
   imports = [
     ./disko.nix
     ./hardware.nix
@@ -13,6 +12,8 @@
   system.stateVersion = "24.05";
 
   networking.hostName = "vps";
+
+  age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1Oy9/d0KtPZSf1bhJjItykOJEz43uPLNpYPdJ8bd8x";
 
   boot.loader.systemd-boot.enable = lib.mkForce false;
 }
