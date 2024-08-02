a19541d36819ca7bb854a6279ae1bdf85af8d59b
Gaetan Lepage <gaetan@glepage.com>
2024-08-02 10:25:16 +0200

    nixos/feroe/wireguard: use module

--- nixos/feroe/wireguard/default.nix
+++ nixos/feroe/wireguard/default.nix
@@ -1,24 +1,8 @@
-{config, ...}: {
+{
   age.secrets.wireguard-private-key.rekeyFile = ./private-key.age;
 
-  networking.wireguard.interfaces.wg0 = {
-    ips = ["10.10.10.23/32"];
-    listenPort = 51820;
-    privateKeyFile = config.age.secrets.wireguard-private-key.path;
-
-    peers = [
-      {
-        endpoint = "glepage.com:51820";
-        publicKey = "jWzlVwkNkaO1uj7Qh+Xemo0EtxIYP2ufK+18oPcdvBY=";
-
-        # Forward all the traffic via VPN.
-        #allowedIPs = [ "0.0.0.0/0" ];
-        # Or forward only particular subnets
-        allowedIPs = ["10.10.10.0/24"];
-
-        # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-        persistentKeepalive = 25;
-      }
-    ];
+  custom.wireguard = {
+    enable = true;
+    ip = "10.10.10.23";
   };
 }
