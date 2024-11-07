4ba9d8e19b9e8da4e9569e5204934a61d88b15f3
Gaetan Lepage <gaetan@glepage.com>
2024-11-07 16:34:53 +0100

    nixos/server/samba: formatting

--- nixos/server/samba.nix
+++ nixos/server/samba.nix
@@ -1,42 +1,35 @@
 {
-  #########
-  # Notes #
-  #########
-  # For a user to be authenticated on the samba server, you must add their password using
+  # WARNING: For a user to be authenticated on the samba server, you must add their password using
   # `smbpasswd -a <user>` as root.
 
-  ########
-  # WSDD #
-  ########
-  # make shares visible for windows 10 clients
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-  };
+  services = {
+    samba = {
+      enable = true;
 
-  ################
-  # samba server #
-  ################
-  services.samba = {
-    enable = true;
+      openFirewall = true;
 
-    openFirewall = true;
+      settings = {
+        lepage = {
+          path = "/tank/lepage_family/";
+          public = "no";
+          writable = "yes";
+          "guest ok" = "no";
+          "valid users" = "anne-catherine lepage tanguy";
+        };
 
-    settings = {
-      lepage = {
-        path = "/tank/lepage_family/";
-        public = "no";
-        writable = "yes";
-        "guest ok" = "no";
-        "valid users" = "anne-catherine lepage tanguy";
+        share = {
+          path = "/tank/share/";
+          public = "yes";
+          writable = "yes";
+          "guest ok" = "yes";
+        };
       };
+    };
 
-      share = {
-        path = "/tank/share/";
-        public = "yes";
-        writable = "yes";
-        "guest ok" = "yes";
-      };
+    # make shares visible for windows clients
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
     };
   };
 }
