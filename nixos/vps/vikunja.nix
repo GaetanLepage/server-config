c935f8fcdf8410956ca3022b15ab3859be3de3a2
Gaetan Lepage <gaetan@glepage.com>
2025-01-10 17:39:49 +0100

    vps/vikunja: periodically backup the database

--- nixos/vps/vikunja.nix
+++ nixos/vps/vikunja.nix
@@ -14,4 +14,31 @@ in
       frontendHostname = domain;
     };
   };
+
+  systemd =
+    let
+      serviceName = "vikunja-backup";
+    in
+    {
+      services.${serviceName} = {
+        description = "Script that periodically backups the vikunja sqlite database to /var/backup";
+
+        serviceConfig = {
+          Type = "oneshot";
+        };
+        script = ''
+          cp -f ${config.services.vikunja.database.path} /var/backup/
+        '';
+      };
+
+      timers.${serviceName} = {
+        wantedBy = [ "timers.target" ];
+        after = [ "multi-user.target" ];
+
+        timerConfig = {
+          OnCalendar = "hourly";
+          Persistent = "yes";
+        };
+      };
+    };
 }
