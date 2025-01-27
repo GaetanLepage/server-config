f7db0f7f1421d6b45dfbad40f077105086be88ab
Gaetan Lepage <gaetan@glepage.com>
2025-01-27 14:33:39 +0100

    nixos/caddy: fancy automatic setting of DNS records in networking.hosts

--- nixos/common/caddy.nix
+++ nixos/common/caddy.nix
@@ -57,5 +57,39 @@
               "reverse_proxy ${proxyStr}";
         }) cfg.reverseProxies;
       };
+
+      networking.hosts =
+        let
+          localWireguardIp = lib.pipe config.networking.wireguard.interfaces.wg0.ips [
+            # [ "10.10.10.x/32" ]
+            lib.head
+            # "10.10.10.x/32"
+            (lib.splitString "/")
+            # [ "10.10.10.x" "32" ]
+            lib.head
+            # "10.10.10.x"
+          ];
+
+          /*
+            "foo.glepage.com"
+            {
+              localIp = "10.10.10.42";
+              vpn = true;
+            };
+
+            [
+              { "10.10.10.42" = [ "foo.glepage.com" ]; }
+            ]
+          */
+          mkDnsMapping =
+            domain: opts:
+            let
+              wireguardIp = if opts.localIp == "localhost" then localWireguardIp else opts.localIp;
+            in
+            lib.optional opts.vpn { ${wireguardIp} = domain; };
+
+          reverseProxies' = lib.concatLists (lib.mapAttrsToList mkDnsMapping cfg.reverseProxies);
+        in
+        builtins.trace (lib.head reverseProxies') builtins.zipAttrsWith (n: v: v) reverseProxies';
     };
 }
