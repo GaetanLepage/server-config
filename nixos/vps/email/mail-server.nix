b9a864cddd7827bbcdb9d41ea84bff9364efacc8
Gaetan Lepage <gaetan@glepage.com>
2025-04-23 17:49:57 +0200

    mail-server: move accounts to dedicated file

--- nixos/vps/email/mail-server.nix
+++ nixos/vps/email/mail-server.nix
@@ -26,59 +26,7 @@ in
     # Clashes with Adguard
     localDnsResolver = false;
 
-    # To generate the hash:
-    # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
-    loginAccounts = {
-      "gaetan@${base_domain}" = {
-        hashedPassword = "$2b$05$ypXOSOtY8tOzh1c6/G.lkesf9NaiHHFiUi0ZbC/gk1wWlcGtyRCrS";
-        aliases = [
-          "postmaster@${base_domain}"
-          "3iTYog@${base_domain}"
-          "gaetan-medium@${base_domain}"
-          "gaetan-twitter-public@${base_domain}" # @GaetanLepage_
-
-          # Grenug
-          "mail@grenug.fr"
-          "contact@grenug.fr"
-        ];
-      };
-
-      # GitHub notifications & co
-      "dev@${base_domain}" = {
-        hashedPassword = "$2b$05$WSWCiow5WoKz6KbeYM2kDu9up1bs743gqZlt75K.eIkIvWf7.BUDO";
-      };
-
-      "backup-tank@${base_domain}" = {
-        hashedPassword = "$2b$05$4tevC0VlFz25.MbqUJj9IOS0fTuK6FwCoG4SEYosN9hDFiILHaXCS";
-      };
-
-      "nextcloud@${base_domain}" = {
-        hashedPassword = "$2b$05$kM8SVZdLQQcDtKlV4esnnO.Dhs7slPpSn2269fDB4oh6jIDcvWzMy";
-      };
-
-      ################################
-      # Aliases for deleted accounts #
-      ################################
-      "gaetan-inp-org@${base_domain}" = {
-        hashedPassword = "$2b$05$WYcyz0ouNQersRO2M8vfNO3xK3g3p0HPYPJrGwWOMx/CImiDtIvnu";
-      };
-      "gaetan-inp-fr@${base_domain}" = {
-        hashedPassword = "$2b$05$kkIjgZQNey4GT9wtkcL53OjuwXS7gG/fJ586w3vzCEvhPt38WVbOq";
-      };
-      "gaetan-lepage-knives@${base_domain}" = {
-        hashedPassword = "$2b$05$P.hQu3fexLTdofF7KP97wuLf.aG81ChHsiXj43l9rYuQABaC5GrD.";
-      };
-
-      ###########
-      # Famileo #
-      ###########
-      "thierry.famileo@${base_domain}" = {
-        hashedPassword = "$2b$05$p.DRno51jAtjNIxkh47t8Oh9DdhCR2pWBhhjVyDy0X1Z5h78VYmwm";
-      };
-      "francoise.famileo@${base_domain}" = {
-        hashedPassword = "$2b$05$Gww22I/3wTjPzY/f9QzdDeFL/0SgCX8po34QEweP3FRf2dFrRPbVO";
-      };
-    };
+    loginAccounts = import ./accounts.nix;
 
     backup.enable = true;
   };
